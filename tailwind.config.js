86e940c170874f767732d03f2d44c9264cc1f491
azizicoy <azizichoi@gmail.com>
2024-12-02 23:13:30 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      letterSpacing: {
+        lebar: '.5em',
+      },
+      colors: {
+        biru: '#3eccb9',
+        biru2: '#2c9184',
+        hitam: '#171410',
+      },
+    },
+  },
+  plugins: [],
+};
